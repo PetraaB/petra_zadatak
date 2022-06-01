04f727aa4fa653a827fb75fd50e06a2e4dc632bb
unknown <petraberic@ssklivno.net>
2022-06-01 08:00:14 +0200

    new3

--- Petra_zadatak/models.py
+++ Petra_zadatak/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.forms import CharField, ImageField
 
 # Create your models here.
+
+class Karte(models.Model):
+    ime= models.CharField(max_length=50)
+    slika = models.ImageField(upload_to='slike', null=True)
+    tekst = models.TextField(max_length=200)
+    vrijeme=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.ime
