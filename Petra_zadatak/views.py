b3e5da78e94d7be1fc415e460f9085ee6dc37db7
unknown <petraberic@ssklivno.net>
2022-06-01 08:01:38 +0200

    new5

--- Petra_zadatak/views.py
+++ Petra_zadatak/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Karte
 
 # Create your views here.
+
+class home(ListView):
+    template_name = 'generic_cms/home.html'
+    model = Karte
+    ordering = ['-vrijeme']
+    context_object_name = "karte"
